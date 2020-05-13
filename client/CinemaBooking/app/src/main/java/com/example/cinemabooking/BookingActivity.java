8bc9fe9a885b8e12505d34b7137efee0691613fe
Anca <anca.nincu@gmail.com>
2020-05-13 21:47:58 +0300

    Added intend message for BookingActivity class

--- client/CinemaBooking/app/src/main/java/com/example/cinemabooking/BookingActivity.java
+++ client/CinemaBooking/app/src/main/java/com/example/cinemabooking/BookingActivity.java
@@ -2,7 +2,9 @@ package com.example.cinemabooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BookingActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class BookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        Intent intent = getIntent();
+        String movieID = intent.getStringExtra("message1");
+        String movieName = intent.getStringExtra("message2");
+        String movieDates = intent.getStringExtra("message2");
+        String message = movieName + " " + movieID + " " + movieDates;
+
+        TextView textView = (TextView)findViewById(R.id.bookingDataTextView);
+        textView.setText(message);
     }
 }
