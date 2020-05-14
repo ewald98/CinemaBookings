39b409b75763352641bd8a49771ac7f132aecf7a
xellie4 <olah.evelin8@gmail.com>
2020-05-14 11:37:57 +0300

    added booking UI and a little bit of functionality

--- client/CinemaBooking/app/src/main/java/com/example/cinemabooking/BookingActivity.java
+++ client/CinemaBooking/app/src/main/java/com/example/cinemabooking/BookingActivity.java
@@ -4,9 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class BookingActivity extends AppCompatActivity {
+
+    private TextView chosenmovietitle;
+    private TextView chosendate;
+
+    private EditText fullname;
+    private EditText phonenumber;
+    private EditText nrreservedseats;
+
+    private Button bookseatsbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +35,59 @@ public class BookingActivity extends AppCompatActivity {
         String movieName = intent.getStringExtra("message2");
         String movieDates = intent.getStringExtra("message3");
         String message = movieName + " " + movieID + " " + movieDates;
-
+/*
         TextView textView = (TextView)findViewById(R.id.bookingDataTextView);
-        textView.setText(message);
+        textView.setText(message);*/
+
+        TextView movieText = (TextView)findViewById(R.id.chosenmovietitle);
+        movieText.setText(movieName + " " + movieID);
+
+        TextView dateText = (TextView) findViewById(R.id.chosendate);
+        dateText.setText(movieDates);
+
+        fullname = (EditText)findViewById(R.id.fullname);
+        phonenumber = (EditText)findViewById(R.id.phonenumber);
+        nrreservedseats = (EditText)findViewById(R.id.nrreservedseats);
+
+        bookseatsbutton = (Button) findViewById(R.id.bookseatsbutton);
+
+        bookseatsbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = fullname.getText().toString();
+                String phonenr = phonenumber.getText().toString();
+                String seats = nrreservedseats.getText().toString();
+
+                if(name.isEmpty()){
+                    fullname.setError("Please provide full name");
+                    fullname.requestFocus();
+                }
+                if(phonenr.isEmpty()){
+                    phonenumber.setError("Please provide a phone number");
+                    phonenumber.requestFocus();
+                }
+                if(seats.isEmpty()){
+                    nrreservedseats.setError("Please provide a number of seats");
+                    nrreservedseats.requestFocus();
+                }
+                if(!name.isEmpty() && !phonenr.isEmpty() && !seats.isEmpty()){
+                    Log.d("",name);
+
+                    Log.d("",phonenr);
+                    Log.d("",seats);
+                    Toast toast = Toast.makeText(BookingActivity.this, "Booking with success", Toast.LENGTH_LONG);
+                    toast.show();
+
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent goToMainActivity = new Intent(BookingActivity.this, MainActivity.class);
+                            startActivity(goToMainActivity);
+                        }
+                    }, 1000);
+                }
+            }
+        });
     }
 }
